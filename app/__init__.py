f0b7653e0dce8cb907916e09aeccfd76f8a65a1d
Areeq Hasan <areeq.i.hasan@gmail.com>
2021-06-26 14:08:40 -0400

    static folder path

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__, static_url_path="", static_folder="http/client/app/build")
+app = Flask(__name__, static_url_path="", static_folder="./http/client/app/build")
 
 app.config.from_object("app.config")
 app.secret_key = app.config["SECRET_KEY"]
